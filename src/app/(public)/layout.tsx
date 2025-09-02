b3d1489c7a80f2c93e3a88b493dcfb171ec910bd
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2025-09-02 20:33:05 +0200

    deb

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,16 @@
+'use client'
 import Header from "@/components/header";
+import { usePathname } from "next/navigation";
 import type { PropsWithChildren } from "react";
+import { PAGES } from "../config /pages.config";
 
 export default function Layout({children}:
     PropsWithChildren<unknown>){
+        const way = usePathname();
         return(
         <div className="min-h-screen px-4 py-8">
             <Header/>
-            <div className="flex justify-center max-w-[750px] scroll-ps-6">{children}</div>
+            <div className={way === PAGES.HOME ? 'flex justify-center max-w-xl' : 'flex justify-center'}>{children}</div>
         </div>
         )
     }
