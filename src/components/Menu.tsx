4690f010c7848a9f23e2346884dc631b7b0d8ec9
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2025-09-02 22:01:14 +0200

    update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,13 +2,19 @@
 import { PAGES } from "@/app/config /pages.config"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { MENU } from "./menu.data";
+import { MenuItem } from "./MenuItem";
 export default function Menu(){
     const pathname = usePathname() ;
     return(
         <nav className="flex gap-6 text-sm">
-            <Link className={pathname === PAGES.HOME ? 'text-white' : 'text-white/80'} href={PAGES.HOME}>Home</Link>
-            <Link className={pathname === PAGES.EXPLORE ? 'text-white' : 'text-white/80'} href={PAGES.EXPLORE}>Explore</Link>
-            <Link className={pathname === PAGES.PROFILE_FAKE ? 'text-white' : 'text-white/80'}href={PAGES.PROFILE_FAKE}>Profile</Link>
+            {MENU.map(menuItem => (
+            <MenuItem 
+                key={menuItem.name}
+                menuItem={menuItem}
+                isActive={pathname === menuItem.href}
+            />
+            ))}
         </nav>
     )
 }
